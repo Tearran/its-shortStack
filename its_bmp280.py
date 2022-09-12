7c5a527c6131a007f0b0a7d2ceb3692f5e1ee917
Joseph Turner <Tearran@users.noreply.github.com>
2022-09-12 13:06:23 -0700

    Update its_bmp280.py

--- its_bmp280.py
+++ its_bmp280.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from bmp_280 import BMP280
+from lib.bmp_280 import BMP280
 from time import sleep
 
 bmp = BMP280(port=1, mode=BMP280.FORCED_MODE, oversampling_p=BMP280.OVERSAMPLING_P_x16, oversampling_t=BMP280.OVERSAMPLING_T_x1,filter=BMP280.IIR_FILTER_OFF, standby=BMP280.T_STANDBY_1000)
