c93b5bae8b255b420a77eadd42682f2f61985811
Joseph Turner <Tearran@users.noreply.github.com>
2022-09-12 12:43:52 -0700

    Add files via upload
    BMP280 Temperature and Pressure

---
+++ its_bmp280.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+from bmp_280 import BMP280
+from time import sleep
+
+bmp = BMP280(port=1, mode=BMP280.FORCED_MODE, oversampling_p=BMP280.OVERSAMPLING_P_x16, oversampling_t=BMP280.OVERSAMPLING_T_x1,filter=BMP280.IIR_FILTER_OFF, standby=BMP280.T_STANDBY_1000)
+
+pressure = bmp.read_pressure() 
+temp = bmp.read_temperature()
+tempf = temp * 1.8 + 32 
+
+pressure = str(round(pressure,2 ))
+temp = str(round(temp, 2))
+tempf = str(round(tempf, 1 ))
+
+print(pressure + " hPa: ")
+print(tempf + " °F: ")
+print(temp + " °C: ")
