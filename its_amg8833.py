2dbf464d423afa9e91195bb109350d2844462e47
Joseph Turner <Tearran@users.noreply.github.com>
2022-09-15 12:26:03 -0700

    Update its_amg8833.py

--- its_amg8833.py
+++ its_amg8833.py
@@ -4,6 +4,7 @@
 import time
 from lib.amg8833 import AMGGridEye
 
+# ini amg8833 lib
 sensor = AMGGridEye()
 sensor.reset_flags_and_settings()
 time.sleep(.1)
@@ -11,10 +12,10 @@ sensor.set_fps_mode(AMGGridEye.FPS_10_MODE)
 
 try:  
 #while(True):
-    #update temperature
-    sensor.update_pixel_temperature_matrix()
-    time.sleep(.1)
+  #update temperature
+  sensor.update_pixel_temperature_matrix()
+  time.sleep(.1)
   
 finally:
   print(sensor.get_pixel_temperature_matrix())
-  print(str(round(sensor.get_thermistor_temperature(), 0))) 
+  #print(str(round(sensor.get_thermistor_temperature(), 0))) 
